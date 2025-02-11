3ec3535d5a50364bb6e75a6441189341e59b523c
Lachlan Hetherton <het018@csiro.au>
2025-02-11 11:38:58 +1100

    Removed workspace.cfg from the setup.py file.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='csiro_workspace',
-      version='0.1',
+      version='0.2',
       description='Workspace Python Module',
       author='CSIRO',
       author_email='workspace@csiro.au',
-      url='http://research.csiro.au/workspace',
+      url='https://github.com/csiro-workspace/workspace-python',
       packages=['csiro_workspace'],
-      package_data={'csiro_workspace': ['workspace.cfg']}
+      package_data={}
 )
 
